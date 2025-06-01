7ca7ca1482c8d87915552a0d43461fcc2f0508e6
nicolas.izraelski <nicolas.izraelski@possumus.tech>
2025-06-01 16:58:58 -0300

    Enhance CORS configuration in MiddlewaresConfig.ts by specifying allowed methods and headers, and adding a middleware to set CORS headers for all responses.

--- src/server/MiddlewaresConfig.ts
+++ src/server/MiddlewaresConfig.ts
@@ -5,9 +5,20 @@ import cors from "cors";
 const ConfigureServerMiddlewares = (app: Application) => {
   const corsOptions = {
     origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin"],
     optionsSuccessStatus: 200,
+    preflightContinue: false,
   };
+
   app.use(cors(corsOptions));
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept, Origin");
+    next();
+  });
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
